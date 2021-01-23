3900dea6383c50caf5ed0a6fdbcb034e157d8e08
maylacdao <may.lacdao@gmail.com>
2021-01-23 10:26:43 -0500

    Update app.js

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -3,46 +3,30 @@ function buildMetadata(sample) {
     d3.json("samples.json").then((data) => {
         //console.log(data);
         var metadata = data.metadata;
-        var resultsArray = metadata.filter(sampleobject => sampleobject.id == sample);
-        var result = resultsArray[0];
+        var resultsarray = metadata.filter(sampleobject => sampleobject.id == sample);
+        var result = resultsarray[0];
         var PANEL = d3.select("#sample-metadata");
         PANEL.html("");
         Object.entries(result).forEach(([key, value]) => {
-            PANEL.append("h6").text(`${key}:${value}`);
+            PANEL.append("h6").text(`${key}: ${value}`);
         });
     });
 };
 
 
-//Create function to build horizontal bar chart
-function buildCharts() {
+//Create function to build charts
+function buildCharts(sample) {
     //Load sample data using d3.js
     d3.json("samples.json").then((data) => {
         var samples = data.samples;
-        var resultsArray = samples.filter(sampleobject => sampleobject.id == samples);
-        
-        var result = resultsArray[0];
+        var resultsarray = samples.filter(sampleobject => sampleobject.id == samples);
+        var result = resultsarray[0];
         console.log(result);
 
         var ids = result.otu_ids;
         var labels = result.otu_labels;
         var values = result.sample_values;
 
-        //Build horizontal bar chart using sample data
-        var horizontalBarData = [{
-            y: ids.slice(0,10).map(otuID => `OTU ${otuID}`).reverse(),
-            x: values.slice(0,10).reverse(),
-            text: labels.slice(0,10).reverse(),
-            type:"bar",
-            orientation: "h"
-        }];
-
-        var horizontalBarLayout = {
-            title: "Top 10 Microbial Species per Individual"
-        };
-
-        Plotly.newPlot("bar", horizontalBarData, horizontalBarLayout);
-        
         //Build bubble chart using sample data
         var bubbleChartData = [{
             x: ids,
@@ -63,6 +47,21 @@ function buildCharts() {
 
         Plotly.newPlot("bubble", bubbleChartData, bubbleChartLayout);
     });
+        //Build horizontal bar chart using sample data
+        var horizontalBarData = [{
+            y: ids.slice(0,10).map(otuID => `OTU ${otuID}`).reverse(),
+            x: values.slice(0,10).reverse(),
+            text: labels.slice(0,10).reverse(),
+            type:"bar",
+            orientation: "h"
+        }];
+
+        var horizontalBarLayout = {
+            title: "Top 10 Microbial Species per Individual"
+        };
+
+        Plotly.newPlot("bar", horizontalBarData, horizontalBarLayout);
+
 }
 
 function init() {
@@ -72,23 +71,22 @@ function init() {
     //Populate options using sample names
     d3.json("samples.json").then((data) => {
         var sampleNames = data.names;
-        sampleNames.forEach((sample) => {
-            selector
+        sampleNames.forEach((sample) =>     selector
                 .append("option")
                 .text(sample)
                 .property("value", sample);
         });
 
-        //Build initial plots using first sample entry
+        //Build plots using first sample entry
         const firstEntry = sampleNames[0];
-        buildCharts(firstEntry);
+        buildCharts();
         buildMetadata(firstEntry);
     });
 }
 
 function optionChanged(newSample) {
     //Fetch new data whenever a different option is selected
-    buildCharts(newSample);
+    buildCharts();
     buildMetadata(newSample);
 }
 
