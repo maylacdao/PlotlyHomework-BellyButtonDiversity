7a55daff50a76f18285b8a97b072e3deb56f175d
maylacdao <may.lacdao@gmail.com>
2021-01-23 08:44:01 -0500

    Update app.js

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -3,3 +3,13 @@ d3.json("samples.json").then((data) => {
     console.log(data);
 });
 
+//Create function to build horizontal bar chart
+function buildHorizontalBarChart() {
+    var trace1 
+
+    var data 
+
+    var layout
+
+    Plotly.newPlot("bar", data, layout)
+}
